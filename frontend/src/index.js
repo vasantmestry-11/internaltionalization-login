010ec13bf5293c10cea7a480942dc33449c64dea
Vasant Mestry <Vasant.Mestry@Xoriant.Com>
2023-03-21 12:51:33 +0530

    :sparkles: :art: react-18next initial setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./index.css";
+
+i18n
+  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+  // learn more: https://github.com/i18next/i18next-http-backend
+  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
+  .use(Backend)
+  .use(LanguageDetector) // to detect language of browser
+  .use(initReactI18next) // initializes react-i18next
+  .init({
+    supportedLanguages: ["en"],
+    fallbackLng: "en",
+    detection: {
+      // order and from where user language should be detected. Default cookie name is: i18next
+      order: ["path", "cookie", "htmlTag", "localStorage", "sessionStorage"],
+      caches: ["cookie"],
+    },
+    backend: {
+      loadPath: "/assets/locales/{{lng}}/translation.json", //path where resources get loaded from
+    },
+  });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const LoadingMarkup = () => (
+  <div className="py-4 text-center text-white">
+    <h1>Loading...</h1>
+  </div>
+);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Suspense fallback={<LoadingMarkup />}>
+        <App />
+      </Suspense>
+    </HashRouter>
   </React.StrictMode>
 );
